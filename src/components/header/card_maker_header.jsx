aab2c8fb2df06b0b690e7ce87e5aeb8039a1e18a
rainlee <rainlee4401@gmail.com>
2021-08-19 13:38:43 +0900

    header logout button add

--- src/components/header/card_maker_header.jsx
+++ src/components/header/card_maker_header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./card_maker_header.module.css";
 
-const CardMakerHeader = () => {
+const CardMakerHeader = ({ loginState }) => {
   return (
     <header className={styles.header}>
+      {!loginState && (
+        <div className={styles.logout__container}>
+          <button className={styles.logout__btn}>Logout</button>
+        </div>
+      )}
       <img className={styles.logo} src="/images/logo.png" alt="logo" />
       <h1 className={styles.title}>Business Card Maker</h1>
     </header>
